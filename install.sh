e1506d2038840dc0cbf4609a67d5ab69f75293df
Aloxo <56530679+alohane@users.noreply.github.com>
2023-06-11 13:50:09 +0700

    Update XrayR release

--- install.sh
+++ install.sh
@@ -115,7 +115,7 @@ install_XrayR() {
             exit 1
         fi
         echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
-        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/zeropanel/XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://zero.fuzzypn.me/download/xrayr_stable.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
@@ -126,7 +126,7 @@ install_XrayR() {
 	else
 	    last_version="v"$1
 	fi
-        url="https://github.com/zeropanel/XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip"
+        url="https://zero.fuzzypn.me/download/xrayr_stable.zip"
         echo -e "开始安装 XrayR ${last_version}"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
